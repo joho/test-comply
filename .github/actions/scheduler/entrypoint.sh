2605580241e45599050da77ab4e16eb917528507
John Barton (joho) <jrbarton@gmail.com>
2019-09-19 13:48:46 +1000

    Try a thing

--- .github/actions/scheduler/entrypoint.sh
+++ .github/actions/scheduler/entrypoint.sh
@@ -3,11 +3,13 @@
 cd $GITHUB_WORKSPACE
 
 if [ -n "$INPUT_JIRA_USERNAME" ]; then
-   sed -i "s/<JIRA_USERNAME>/$INPUT_JIRA_USERNAME/g" comply.yml
+    echo "Setting JIRA username"
+    sed -i "s/\<JIRA_USERNAME\>/$INPUT_JIRA_USERNAME/g" comply.yml
 fi
 
 if [ -n "$INPUT_JIRA_PASSWORD" ]; then
-   sed -i "s/<JIRA_PASSWORD>/$INPUT_JIRA_PASSWORD/g" comply.yml
+    echo "Setting JIRA username"
+    sed -i "s/\<JIRA_PASSWORD\>/$INPUT_JIRA_PASSWORD/g" comply.yml
 fi
 
 comply scheduler
